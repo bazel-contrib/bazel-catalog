cd3c597cfb0da25fe8a03498c0d2d8048dd108da
Alex Eagle <alex@aspect.dev>
2023-07-31 18:37:25 -0700

    chore: remove scoring for now, it's not good enough to launch

--- filters/stability_score.jq
+++
@@ -1,9 +0,0 @@
-# Overall assessment "is this repo stable", based on data supplied as arguments
-
-[
-    # Pre-release is a red flag
-    if $pre_1_0 == "true" then -1 else 0 end
-
-    # TODO: need more inputs to give better scoring
-] | add
-| if . > 0 then "Stable" elif . < 0 then "Unstable" else "Unclear" end
--- scripts/generate_ghpages.sh
+++ scripts/generate_ghpages.sh
@@ -19,7 +19,7 @@ do
     IFS=$',' read -r subscribers_count stargazers_count forks_count open_issues_count repo_age_days < <(
         jq --raw-output --from-file "$SCRIPT_DIR/../filters/repo_data.jq" "$SCRIPT_DIR/../stats/$ghrepo/repo.json"
     )
-    IFS=$',' read -r last_tag pre_1_0 < <(
+    IFS=$',' read -r last_tag < <(
         jq --raw-output --from-file "$SCRIPT_DIR/../filters/tag_data.jq" < "$SCRIPT_DIR/../stats/$ghrepo/tags.json"
     )
     IFS=$',' read -r total_downloads last_release_age_days < <(
@@ -30,27 +30,11 @@ do
     downloads_per_day=$((total_downloads / repo_age_days))
     health_percentage=$(jq --raw-output '.health_percentage' "$SCRIPT_DIR/../stats/$ghrepo/community_profile.json")
 
-    # Run our scoring algorithms
-    adoption_score=$(
-        jq --null-input --raw-output --arg stars "$stargazers_count" --arg forks "$forks_count" --arg downloads "$downloads_per_day" --from-file "$SCRIPT_DIR/../filters/adoption_score.jq"
-    )
-    maintenance_score=$(
-        jq --null-input --raw-output --arg last_release_age_days "$last_release_age_days" --from-file "$SCRIPT_DIR/../filters/maintenance_score.jq"
-    )
-    quality_score=Unknown
-    stability_score=$(
-        jq --null-input --raw-output --arg pre_1_0 "$pre_1_0" --from-file "$SCRIPT_DIR/../filters/stability_score.jq"
-    )
-
     # Render the output
     cat > "$DESTDIR"/index.md <<EOF
 # $ghrepo
 
 ## Adoption
-
-![GitHub all releases](https://img.shields.io/github/downloads/$ghrepo/total)
-![GitHub forks](https://img.shields.io/github/forks/$ghrepo)
-![GitHub stars](https://img.shields.io/github/stars/$ghrepo)
 
 - Subscribers: ${subscribers_count}
 - Stargazers: ${stargazers_count}
@@ -61,15 +45,7 @@ do
 
 > Note, no downloads likely means this is distributed using source tarballs instead.
 
-Score: ${adoption_score}
-
 ## Maintenance
-
-![GitHub contributors](https://img.shields.io/github/contributors/$ghrepo)
-![GitHub last commit (by committer)](https://img.shields.io/github/last-commit/$ghrepo)
-![GitHub Release Date - Published_At](https://img.shields.io/github/release-date/$ghrepo)
-![GitHub issues](https://img.shields.io/github/issues/$ghrepo)
-![GitHub pull requests](https://img.shields.io/github/issues-pr/$ghrepo)
 
 - Community Health: ${health_percentage}%
 - Open issues: ${open_issues_count}
@@ -78,20 +54,9 @@ Score: ${adoption_score}
 > Note, a large number of issues may indicate that maintainers don't correct defects, but might also
 > just be an indicator of lots of users.
 
-Score: ${maintenance_score}
-
-## Quality
-
-*No data yet*
-
 ## Stability
 
-![GitHub tag (with filter)](https://img.shields.io/github/v/tag/$ghrepo)
-
 Most recent tag: ${last_tag}
-Pre 1.0: ${pre_1_0}
-
-Score: ${stability_score}
 
 ## More Data
 
@@ -100,7 +65,7 @@ Score: ${stability_score}
 EOF
 
     # Finally record our result in the table on the homepage.
-    echo "| [${ghrepo}](./${ghrepo}) | $adoption_score | $maintenance_score | $quality_score | $stability_score |" >> "$OUTPUT_ROOT"/index.md
+    echo "| [${ghrepo}](./${ghrepo}) | ![GitHub all releases](https://img.shields.io/github/downloads/$ghrepo/total) ![GitHub forks](https://img.shields.io/github/forks/$ghrepo) ![GitHub stars](https://img.shields.io/github/stars/$ghrepo) | ![GitHub contributors](https://img.shields.io/github/contributors/$ghrepo) ![GitHub last commit (by committer)](https://img.shields.io/github/last-commit/$ghrepo) ![GitHub Release Date - Published_At](https://img.shields.io/github/release-date/$ghrepo) ![GitHub issues](https://img.shields.io/github/issues/$ghrepo) ![GitHub pull requests](https://img.shields.io/github/issues-pr/$ghrepo) | ![GitHub tag (with filter)](https://img.shields.io/github/v/tag/$ghrepo) |" >> "$OUTPUT_ROOT"/index.md
 done < <(
     jq --raw-output \
     --from-file "${SCRIPT_DIR}"/../filters/ghrepo.jq \
